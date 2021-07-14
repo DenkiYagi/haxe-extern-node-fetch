07e094164528aaabd526ad82af27d3bf13b36011
terurou <terurou@denkiyagi.jp>
2021-07-14 23:00:43 +0900

    Add `nodefetch_always_require`

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ haxelib install node-fetch
 ## Requirements
 
 Haxe 4.x
+
+## Compiler Flags
+
+### `nodefetch_always_require`
+
+`NodeFetch.fetch()` is always compiled to `require("node-fetch")()`.
--- src/js/npm/NodeFetch.hx
+++ src/js/npm/NodeFetch.hx
@@ -6,7 +6,14 @@ import js.npm.nodefetch.Response;
 
 @:jsRequire("node-fetch")
 extern class NodeFetch {
+    #if nodefetch_always_require
+    public static inline function fetch(url:RequestInfo, ?init:RequestInit):Promise<Response> {
+        return js.Lib.require("node-fetch")(url, init);
+    }
+    #else
     @:selfCall
     static function fetch(url:RequestInfo, ?init:RequestInit):Promise<Response>;
+    #end
+
     static function isRedirect(code:Int):Bool;
 }
